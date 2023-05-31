b488f9a8c9c06088f70813d99216c6cc47d4224c
Boris Fritscher <boris@fritscher.ch>
2023-05-31 09:58:32 +0200

    rename MessageModel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Message
 
 @admin.register(Message)
-class ModelAdmin(admin.ModelAdmin):
+class MessageModelAdmin(admin.ModelAdmin):
     list_display = ('pk', 'subject', 'created_at', 'updated_at')
     list_filter = ('created_at',)
     search_fields = ('subject', 'body')
