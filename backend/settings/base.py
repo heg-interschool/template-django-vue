901c3fcda02fc0d2351a6b5e01d9f265a1f6550c
Boris Fritscher <boris@fritscher.ch>
2023-06-02 10:20:27 +0200

    Update base.py disable basic auth

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+       # 'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
