a4e896507caa86fae33b01cc2b38d6f94240ac9f
Boris Fritscher <boris@fritscher.ch>
2026-04-27 19:21:23 +0200

    FIx: python 3.14 deadlock on runserver with reload

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.api'
+
+    def ready(self):
+        # Workaround for Python 3.14 importlib deadlock with Django runserver thread and allauth
+        try:
+            import allauth.account.internal.constants
+        except ImportError:
+            pass
